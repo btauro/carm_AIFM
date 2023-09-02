9c01939c4a294736939b47e04c32941e69e0defd
btauro <btauro@node-0.btauro-163111.rmt-pg0.utah.cloudlab.us>
2023-09-01 21:59:58 -0600

    update table updates

--- aifm/src/manager.cpp
+++ aifm/src/manager.cpp
@@ -444,7 +444,7 @@ void GCParallelMarker::slave_fn(uint32_t tid) {
 
             if (!ptr->meta().is_shared()) {
               ptr->meta().set_evacuation();
-	    			  //update_cache_object(ptr);
+	      //update_cache_object(ptr);
             } else {
               reinterpret_cast<GenericSharedPtr *>(ptr)->traverse(
                   [](GenericSharedPtr *ptr) { ptr->meta().set_evacuation(); });
@@ -514,6 +514,7 @@ void GCParallelWriteBacker::slave_fn(uint32_t tid) {
             auto *ptr =
                 reinterpret_cast<GenericFarMemPtr *>(obj.get_ptr_addr());
             manager->swap_out(ptr, obj);
+	    //update_cache_object(ptr);
           }
         }
         cur += helpers::align_to(obj.size(), sizeof(FarMemPtrMeta));
